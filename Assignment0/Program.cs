2eac8d5fa5662cb50aba2ade9df598faf811015e
luhj <luhj@itu.dk>
2021-09-05 11:35:40 +0200

    Created user interface for IsLeapYea

--- Assignment0/Program.cs
+++ Assignment0/Program.cs
@@ -6,7 +6,10 @@ namespace Assignment0
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worl!");
+            System.Console.WriteLine("Enter a year and hit [Enter]");
+            int year = Int32.Parse(Console.ReadLine());
+            System.Console.WriteLine(isLeapYear(year) ? "yay" : "nay");
+            
         }
 
         public static bool isLeapYear(int year) {
