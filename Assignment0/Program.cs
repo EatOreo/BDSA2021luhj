b878e0fd51807eefee3a9b2b45ebacdbc489dd0f
luhj <luhj@itu.dk>
2021-09-08 11:41:00 +0200

    Implemented error handling

--- Assignment0/Program.cs
+++ Assignment0/Program.cs
@@ -6,13 +6,21 @@ namespace Assignment0
     {
         public static void Main(string[] args)
         {
-            System.Console.WriteLine("Enter a year and hit [Enter]");
-            int year = Int32.Parse(Console.ReadLine());
-            System.Console.WriteLine(isLeapYear(year) ? "yay" : "nay");
-            
+            while (true) {
+                System.Console.WriteLine("Enter a year and hit [Enter]");
+                try {
+                    string input = Console.ReadLine();
+                    if (input == "end") break;
+                    int year = Int32.Parse(input);
+                    System.Console.WriteLine(isLeapYear(year) ? "yay" : "nay");
+                } catch (Exception e) {
+                    System.Console.WriteLine(e.Message);
+                }
+            }
         }
 
         public static bool isLeapYear(int year) {
+            if (year < 1582) throw new Exception("The leap year function should only apply to years from 1582.");
             return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
         }
     }
